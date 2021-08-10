2557a7e485c263dde05a203ca4fbc7934a3970c1
pputnam-NAVEX <philip.putnam@navexglobal.com>
2021-08-10 16:45:21 -0700

    breakout text into small component to test

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <h2>Hello from React!</h2>
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+function App() {
+    return (
+        <div className="App">
+            <h2>Hello from React!</h2>
+        </div>
+    );
+};
+
+export default App;
